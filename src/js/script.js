3e9c74698cd214549797707da844927c3b591bf4
Anya <likamoon@list.ru>
2020-10-14 01:45:12 +0300

    Основная разметка + половина блоков

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,33 @@
-$(document).ready(function () {    
+$(document).ready(function () {   
+    
+    $('#promo-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        appendDots: $('.promo__slider-dots'),
+        rows: 0,
+        prevArrow: '#promo-slider-prev',
+        nextArrow: '#promo-slider-next'
+    });
+
+    $('.slider-items').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<button class="slider-items__arrow slider-items__arrow--left" aria-label="Previous" type="button" style=""></button>',
+        nextArrow: '<button class="slider-items__arrow slider-items__arrow--right" aria-label="Previous" type="button" style=""></button>',
+        rows: 0,
+
+        responsive: [
+            {
+                breakpoint: 1380,
+                settings: {
+                    slidesToShow: 3
+                }
+            }
+        ]
+    });
 
 })
 
